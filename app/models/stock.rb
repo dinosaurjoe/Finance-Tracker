47bc20bb11077fcec869a584100dd06bd48d6f56
Joe Schafer <joeschaferastula@gmail.com>
2018-05-24 15:20:59 -0500

    new stock lookup based on gem changes

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,25 +1,21 @@
 class Stock < ApplicationRecord
   has_many :user_stocks
   has_many :users, through: :user_stocks
-  
+
   def self.find_by_ticker(ticker_symbol)
     where(ticker: ticker_symbol).first
   end
-  
+
   def self.new_from_lookup(ticker_symbol)
-    begin 
-      looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-      
-    
-      price = strip_commas(looked_up_stock.l)
-      new(name: looked_up_stock.name, ticker: looked_up_stock.symbol, last_price: price)
-    rescue Exception => e 
-      return nil
-    end
+    looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+    return nil unless looked_up_stock.company_name
+    new_stock = new(ticker: looked_up_stock.symbol, name: looked_up_stock.company_name)
+    new_stock.last_price = looked_up_stock.latest_price
+    new_stock
   end
-  
+
   def self.strip_commas(number)
-    number.gsub(",", "")   
+    number.gsub(",", "")
   end
-  
+
 end
