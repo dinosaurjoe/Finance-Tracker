97cb3cb2c3119dc9726aaca1c55a6a1fc585bb0f
Joe Schafer <joeschaferastula@gmail.com>
2018-05-24 20:01:03 -0500

    placeholder object added

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,2 +1,14 @@
 var app = angular.module('FinanceTrackerApp',[]);
-                 .controller('stocksController', ['$scope', function($scope){}])
+                 .controller('stocksController', ['$scope', function($scope){
+                  $scope.lookup = function(){
+                    if($scope.ticker != undefined && $scope.ticker != ''){
+                        $scope.stock = {
+                          symbol: 'FOO',
+                          name: 'Example Corp',
+                          last_price: '123.00'
+                        }
+                    } else {
+                        $scope.stock = {}
+                    }
+                   }
+                 }])
