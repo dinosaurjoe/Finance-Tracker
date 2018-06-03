c05a66becfb0e70bb0e057389c51310be544b204
Joe Schafer <joeschaferastula@gmail.com>
2018-06-03 08:26:55 -0600

    modified stock model to include can be added attribute

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -5,7 +5,9 @@ class StocksController < ApplicationController
       @stock ||= Stock.new_from_lookup(params[:stock])
     end
     if @stock
-      render json: @stock
+      @stock.can_be_added = current_user.can_add_stock?(@stock.ticker)
+
+      render json: @stock, methods: [:can_be_added]
     else
       render status: 404, json: { response: 'No stock exists for this symbol.' }
     end
